9e0530b6ec9fc193d4b72da04413ee6daa7db403
Jose Garcia <betankore@gmail.com>
2023-09-07 16:16:54 -0400

    Add nvim configs

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,35 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'tsserver',
+  'eslint',
+  'volar',
+})
+
+
+local cmp = require('cmp')
+
+cmp.setup({
+    -- Preselect first item from autocompletion
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    -- Sets the key Enter to select completion
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+    }
+})
+
+
+local lspconfig = require('lspconfig')
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup()
---
+++ .config/nvim/init.lua
@@ -0,0 +1,19 @@
+
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
+
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.clipboard = "unnamedplus"
