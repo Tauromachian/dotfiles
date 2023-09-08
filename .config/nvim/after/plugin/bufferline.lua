ee6a9c817a23d56249f01fb2ce733dc811aee7a7
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-08 10:57:29 -0400

    Configure buffer lin

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,1 +1,22 @@
-require("bufferline").setup{}
+local bufferline = require('bufferline')
+
+bufferline.setup {
+    options = {
+        numbers = 'buffer_id',
+        always_show_bufferline = false,
+        show_buffer_icons = true,
+        color_icons = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+
+                separator = true
+            }
+        },
+
+    }
+}
+
+
