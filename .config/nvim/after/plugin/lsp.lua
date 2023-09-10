af9a92758543fbdd53f6fa682742ce2ef9a708ec
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-09 20:06:48 -0400

    Add astro server

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,6 +5,7 @@ lsp.ensure_installed({
   'tsserver',
   'eslint',
   'volar',
+    'astro'
 })
 
 
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "php" },
+  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "php", "astro" },
 
     indent = { enable = true },
 
