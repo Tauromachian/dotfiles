48fa68de9cfe5c48f2f8dc3919a9ce88c51f6d48
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-14 09:32:38 -0400

    Autoformat on save

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,7 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.ensure_installed({
-  -- Replace these with whatever servers you want to install
+    -- Replace these with whatever servers you want to install
     'tsserver',
     'eslint',
     'volar',
@@ -20,31 +20,26 @@ cmp.setup({
     },
     -- Sets the key Enter to select completion
     mapping = {
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }
 })
 
 
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-lspconfig.intelephense.setup{}
+lspconfig.intelephense.setup {}
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({ buffer = bufnr })
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        callback = function()
+            vim.lsp.buf.format()
+        end
+    })
 end)
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
 
 lsp.setup()
