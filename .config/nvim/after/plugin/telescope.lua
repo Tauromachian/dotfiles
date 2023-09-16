09c99c737c265cfcc786517d75a052745ca97f32
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-15 21:36:58 -0400

    Open file in nvim tree when found in telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,5 +2,21 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ts', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+local actions = require('telescope.actions')
+require('telescope').setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<CR>"] = function(prompt_bufnr)
+                    actions.select_default(prompt_bufnr)
+                    vim.cmd(":NvimTreeFindFile")
+                    vim.api.nvim_input('<CR>')
+                    vim.cmd(":NvimTreeCollapseKeepBuffers")
+                end,
+            },
+        },
+    },
+}
