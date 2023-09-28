40ee06ed8e93622bf38692249447089926a9d96f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-28 10:01:24 -0400

    Add formatter configurations

---
+++ .config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,33 @@
+local function prettier()
+    return {
+        exe = "npx",
+        args = { "prettier", "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+        stdin = true
+    }
+end
+
+require('formatter').setup {
+    filetype = {
+        javascript = { prettier },
+        typescript = { prettier },
+        javascriptreact = { prettier },
+        typescriptreact = { prettier },
+        vue = { prettier },
+        ['javascript.jsx'] = { prettier },
+        ['typescript.tsx'] = { prettier },
+        markdown = { prettier },
+        css = { prettier },
+        json = { prettier },
+        jsonc = { prettier },
+        scss = { prettier },
+        less = { prettier },
+        yaml = { prettier },
+        graphql = { prettier },
+        html = { prettier },
+    }
+}
+
+vim.cmd [[augroup FormatAutogroup]]
+vim.cmd [[autocmd!]]
+vim.cmd [[autocmd BufWritePost *.js,*.vue,*.json,*.html,*.astro,*.css,*.scss,*.css FormatWrite]]
+vim.cmd [[augroup END]]
