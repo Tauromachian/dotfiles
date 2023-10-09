50035917dd38b355fbaf836e6e1b4a8ee21aacea
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-10-09 15:48:16 -0400

    Remove unnecessary function

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tgf', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ts', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
 
 -- The code bellow opens in nvim tree the file location after the same is found in telescope
 local actions = require('telescope.actions')
