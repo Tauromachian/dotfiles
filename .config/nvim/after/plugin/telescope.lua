b6fd7b9230dd53b8c58535fa42d90355612e51e8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-10-09 15:50:15 -0400

    Add shortcut for displaying buffers

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tgf', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 -- The code bellow opens in nvim tree the file location after the same is found in telescope
