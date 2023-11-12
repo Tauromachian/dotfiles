c8f5ac13be703bb39ae7911f59604ce9306c15d6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-11-12 09:10:39 -0500

    Add mapping for jumping between hunks

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -8,6 +8,20 @@ require('gitsigns').setup {
             vim.keymap.set(mode, l, r, opts)
         end
 
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, { expr = true })
+
+
         -- Actions
         map('n', '<leader>gs', gs.stage_hunk)
         map('n', '<leader>gr', gs.reset_hunk)
