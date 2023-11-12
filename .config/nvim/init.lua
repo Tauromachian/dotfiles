6a420a71c6c10cd08e26a6d96e0ee6912f853fc4
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-11-12 08:17:23 -0500

    Add simple search shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,5 +20,8 @@ vim.opt.scrolloff = 8
 
 vim.opt.clipboard = "unnamedplus"
 
+-- Remaps
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
