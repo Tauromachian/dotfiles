f0e3bff1e747507584b8b54c0e2ac50341e93f6a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-11-29 09:17:33 -0500

    Move true color activation above rempas

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,14 +19,15 @@ vim.opt.scrolloff = 8
 
 vim.opt.clipboard = "unnamedplus"
 
--- Remaps
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 if vim.fn.has('termguicolors') == 1 then
     vim.opt.termguicolors = true
     vim.cmd [[let &t_8f="\e[38;2;%lu;%lu;%lum"]]
     vim.cmd [[let &t_8b="\e[48;2;%lu;%lu;%lum"]]
 end
+
+
+-- Remaps
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
