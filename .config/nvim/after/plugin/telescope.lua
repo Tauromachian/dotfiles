a8f7186da969ca345f46d2bc4f72cd0dd57a65fb
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-11-30 15:48:07 -0500

    Add function to close buffers on telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,26 @@
 local builtin = require('telescope.builtin')
+local action_state = require('telescope.actions.state')
+
+-- This function allows to close buffers on the buffer search.
+local buffer_searcher = function()
+    builtin.buffers {
+        attach_mappings = function(prompt_bufnr, map)
+            local delete_buf = function()
+                local selection = action_state.get_selected_entry()
+                vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+            end
+
+            -- this is just an example
+            map('n', 'dd', delete_buf)
+
+            return true
+        end
+    }
+end
+
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>tb', buffer_searcher, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 -- The code bellow opens in nvim tree the file location after the same is found in telescope
