debf291d84874d226a61a037a7657ad50b733708
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-12-01 20:24:18 -0500

    Make basic config for nvim diagnostics

---
+++ .config/nvim/after/plugin/diagnostics.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_set_keymap('n', '<leader>ep', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+
+
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
