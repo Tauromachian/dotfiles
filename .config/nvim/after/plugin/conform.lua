45dee2409355a045296e5fb8fcbe3b45f41c9922
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-12-04 11:12:04 -0500

    Add conform

---
+++ .config/nvim/after/plugin/conform.lua
@@ -0,0 +1,24 @@
+require("conform").setup({
+    formatters_by_ft = {
+        -- Use a sub-list to run only the first available formatter
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+        markdown = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+        scss = { "prettier" },
+        less = { "prettier" },
+        yaml = { "prettier" },
+        graphql = { "prettier" },
+        html = { "prettier" },
+        astro = { "prettier" },
+        php = { { "pint", "php_cs_fixer" } }
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 1000,
+        lsp_fallback = true,
+    },
+})
