28ab833ba5ef8faa12f192d3f6c26e7374d91bd8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-04 16:25:52 -0500

    Add gpick

--- install-script.sh
+++ install-script.sh
@@ -30,19 +30,6 @@ sudo apt-get install timeshift -y
 
 sudo apt-get update
 sudo apt-get -y install podman
-echo alias docker=podman > ~/.bash_aliases
-
-#Install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '795f976fe0ebd8b75f26a6dd68f78fd3453ce79f32ecb33e7fd087d39bfeb978342fb73ac986cd4f54edd0dc902601dc') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir=bin --filename=composer
-php -r "unlink('composer-setup.php');"
-
-#Install vue globally and update npm
-sudo npm i -g @vue/cli
-sudo npm i -g npm
-sudo npm install -g heroku
-
 
 #Install brave web browser
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
@@ -61,9 +48,8 @@ sudo apt-get install -y samba --install-recommends
 #sudo apt-add-repository -y ppa:teejee2008/ppa
 #sudo apt-get update
 
-#apt applications
-sudo apt-get install -y filezilla -y
-#sudo apt-get install timeshift -y
+# APT
+sudo apt-get -y install gpick
 
 #Gaming
 sudo dpkg --add-architecture i386 
