9c12ea27ffdc8f2faa890d8b8cfa4785105d6a9e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-09 16:34:43 -0500

    Update snipets

--- .config/nvim/lua/plugin/lazy.lua
+++ .config/nvim/lua/plugin/lazy.lua
@@ -33,9 +33,15 @@ require("lazy").setup({
     { 'williamboman/mason-lspconfig.nvim' },
     { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
     { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
+    { "rafamadriz/friendly-snippets" },
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    { 'saadparwaiz1/cmp_luasnip' },
+    {
+        'L3MON4D3/LuaSnip',
+        version = "v2.*",
+        build = "make install_jsregexp"
+    },
 
     -- Git
     'lewis6991/gitsigns.nvim',
