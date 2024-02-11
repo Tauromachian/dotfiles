cf54fc37d4781e4137a95633957702b61006e5eb
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-11 07:23:32 -0500

    Simplify apt installs

--- install-script.sh
+++ install-script.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
-#Install timeshift
-sudo add-apt-repository ppa:teejee2008/ppa
-sudo apt-get update
-sudo apt-get install timeshift -y
-
-#Install docker
-# sudo apt-get update
-
-# sudo apt-get install \
-#     apt-transport-https \
-#     ca-certificates \
-#     curl \
-#     gnupg-agent \
-#     software-properties-common
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# sudo apt-key fingerprint 0EBFCD88
-
-# sudo add-apt-repository \
-#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#    $(lsb_release -cs) \
-#    stable"
-   
-# sudo apt-get update
-
-# sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-sudo apt-get update
-sudo apt-get -y install podman
+sudo apt install -y podman
+sudo apt install -y gpick
+sudo apt install -y gnome-boxes
+sudo apt install -y tmux
+sudo apt install -y nala
 
 #Install brave web browser
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
