def1cacff17011bbdee9f493ca3c4ed00caacd09
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-11 07:22:46 -0500

    Add wine apt install

--- install-script.sh
+++ install-script.sh
@@ -52,7 +52,7 @@ sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-
+sudo apt install --install-recommends winehq-stable
 
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo apt update
