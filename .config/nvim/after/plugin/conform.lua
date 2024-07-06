47c5e9d4eba58b3741a80d3177255970ff3fd25e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-07-06 15:39:56 -0400

    Remove unnecessary comment

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,6 +1,5 @@
 require("conform").setup({
     formatters_by_ft = {
-        -- Use a sub-list to run only the first available formatter
         javascript = { "prettier" },
         typescript = { { "prettier", "biome" } },
         react = { "prettier" },
