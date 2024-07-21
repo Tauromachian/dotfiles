7adc20cbef2a6bcdf99c45f88a24441e4fcbd1b2
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-07-20 21:22:45 -0400

    Make mouse direction script

---
+++ .scripts/mouse_direction.lua
@@ -0,0 +1,20 @@
+local function set_mouse_direction(direction)
+    if direction ~= 'left' and direction ~= 'right' then
+        print('Invalid argument, only "left" or "right" valid')
+        return
+    end
+
+    local direction_string = '1 2 3'
+
+    if direction == 'left' then
+        direction_string = '3 2 1'
+    end
+
+    os.execute("xmodmap -e 'pointer = " .. direction_string .. "'")
+end
+
+if #arg < 1 then
+    print("Usage: lua invert_mouse.lua [left|right]")
+else
+    set_mouse_direction(arg[1])
+end
