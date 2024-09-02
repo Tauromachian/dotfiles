5ad9ba7cd9074b740f9ab384513c951b5f2a1afc
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-09-02 08:44:04 -0400

    Search in hidden files as well

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -39,8 +39,12 @@ buffer_searcher = function()
     }
 end
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', function()
+    builtin.find_files({ hidden = true })
+end, {})
+vim.keymap.set('n', '<leader>fg', function()
+    builtin.live_grep({ additional_args = { "--hidden", "--glob", "!.git/*" } })
+end, {})
 vim.keymap.set('n', '<leader>ft', builtin.colorscheme, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
