88fa7ca35b17761e6931837384e8ce8ab1800f6a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-11-30 11:32:47 -0500

    Highlight neovim tree file when new file is selected

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -3,5 +3,15 @@ require("nvim-tree").setup {
         git_ignored = false,
     }
 }
+local api = require("nvim-tree.api")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if (vim.fn.bufname() == "NvimTree_1") then return end
+
+        api.tree.find_file({ buf = vim.fn.bufnr() })
+    end,
+})
 
 vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
