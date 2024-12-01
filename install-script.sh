b38b5222ea45aaea808ec113ff683ce22cc59059
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-12-01 16:11:31 -0500

    Add curl install

--- install-script.sh
+++ install-script.sh
@@ -5,10 +5,9 @@ sudo apt install -y gnome-boxes
 sudo apt install -y tmux
 sudo apt install -y nala
 sudo apt install -y samba
+sudo apt install -y curl
 
 #Install brave web browser
-sudo apt install curl
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
