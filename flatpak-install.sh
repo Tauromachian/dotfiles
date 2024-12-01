86839fead7d9129e5deb0559060b453bf478a690
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-12-01 17:04:08 -0500

    Remove unnecessary file

--- flatpak-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Install flatpak
-sudo add-apt-repository ppa:flatpak/stable
-sudo apt update
-sudo apt install flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
