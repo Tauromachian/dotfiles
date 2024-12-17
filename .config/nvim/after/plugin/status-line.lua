200cb264d87d77fe98a84b01dcc7a9ef1768d4da
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-12-16 20:01:55 -0500

    Move diagnostics into its own table

--- .config/nvim/after/plugin/status-line.lua
+++ .config/nvim/after/plugin/status-line.lua
@@ -3,13 +3,16 @@ require("lualine").setup {
     sections = {
         lualine_b = {
             {
-                'branch', 'diff', 'diagnostics',
+                'branch',
                 fmt = function(str)
                     if str:len() > 14 then
                         return str:sub(1, 15) .. '...'
                     end
                     return str
                 end,
+            },
+            {
+                'diff'
             }
         },
         lualine_x = {},
