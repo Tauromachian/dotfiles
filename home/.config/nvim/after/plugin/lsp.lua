cb8f1639152ef3f0bed10e7c95d609aff9f58279
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-13 21:55:51 -0400

    Simplify signs code

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -69,14 +69,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-local signs = { Error = "", Warn = "", Hint = "󰌶", Info = "" }
 vim.diagnostic.config({
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = signs.Error,
-            [vim.diagnostic.severity.WARN] = signs.Warn,
-            [vim.diagnostic.severity.HINT] = signs.Hint,
-            [vim.diagnostic.severity.INFO] = signs.Info,
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "󰌶",
+            [vim.diagnostic.severity.INFO] = "",
         },
     },
 })
