a27b9677e42f8d124ec5bd8d0010803d6a91bef0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-13 21:54:07 -0400

    Use new way to define diagnostics signs

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -70,10 +70,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local signs = { Error = "", Warn = "", Hint = "󰌶", Info = "" }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = signs.Error,
+            [vim.diagnostic.severity.WARN] = signs.Warn,
+            [vim.diagnostic.severity.HINT] = signs.Hint,
+            [vim.diagnostic.severity.INFO] = signs.Info,
+        },
+    },
+})
 
 local nvim_lsp = require('lspconfig')
 
