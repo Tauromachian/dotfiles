ee366e395cdfbeb8ae033009df3f28ec0c3240f9
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-15 16:51:24 -0400

    Add deno as a possible formatter for html, json and js

--- home/.config/nvim/after/plugin/conform.lua
+++ home/.config/nvim/after/plugin/conform.lua
@@ -1,6 +1,6 @@
 require("conform").setup({
     formatters_by_ft = {
-        javascript = { "prettier" },
+        javascript = { "prettier", "deno_fmt", stop_after_first = true },
         typescript = { "prettier", "biome", "deno_fmt", stop_after_first = true },
         react = { "prettier" },
         typescriptreact = { "prettier", "biome", stop_after_first = true },
@@ -8,14 +8,14 @@ require("conform").setup({
         vue = { "prettier" },
         markdown = { "prettier" },
         css = { "prettier" },
-        json = { "prettier", "biome", stop_after_first = true },
+        json = { "prettier", "biome", "deno_fmt", stop_after_first = true },
         scss = { "prettier" },
         less = { "prettier" },
         yaml = { "prettier" },
         graphql = { "prettier" },
-        html = { "prettier" },
+        html = { "prettier", "deno_fmt" },
         php = { "php_cs_fixer" },
-        blade = { "blade-formatter" }
+        blade = { "blade-formatter" },
     },
 
 
