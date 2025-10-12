bcc57a8e88f744b059426929464a7d4c243c388f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-10-12 20:29:41 +0200

    Ref/ Rename variables of the quickfix snippet

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -57,15 +57,15 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.keymap.set('n', 'dd', function()
             local qf_list = vim.fn.getqflist()
 
-            local current_line = vim.fn.line('.')
+            local current_line_number = vim.fn.line('.')
 
-            if qf_list[current_line] then
-                table.remove(qf_list, current_line)
+            if qf_list[current_line_number] then
+                table.remove(qf_list, current_line_number)
 
                 vim.fn.setqflist(qf_list, 'r')
 
-                local new_line = math.min(current_line, #qf_list)
-                vim.fn.cursor(new_line, 1)
+                local new_line_number = math.min(current_line_number, #qf_list)
+                vim.fn.cursor(new_line_number, 1)
             end
         end, {
             buffer = true,
