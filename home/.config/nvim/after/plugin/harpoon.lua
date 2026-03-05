a19f4def72ade70213229fcff8a7e60caf60752c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-05 21:09:37 +0100

    Add leader hh to open harpoon

--- home/.config/nvim/after/plugin/harpoon.lua
+++ home/.config/nvim/after/plugin/harpoon.lua
@@ -6,6 +6,7 @@ harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end)
