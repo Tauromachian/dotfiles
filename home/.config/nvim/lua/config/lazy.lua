0a265a8a35e5c0daa8522305130614d22761be7d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-14 19:15:27 +0100

    Load image plugin lazily

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -81,6 +81,7 @@ require("lazy").setup({
     {
         "3rd/image.nvim",
         build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        event = "VeryLazy",
         opts = {
             processor = "magick_cli",
         }
