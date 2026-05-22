77497176547ec95c802b63c2d256ccbe1be3d195
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-05-22 22:42:51 +0200

    Fix/ The status-line was making my editor laggy all of this time bro

--- home/.config/nvim/after/plugin/status-line.lua
+++ home/.config/nvim/after/plugin/status-line.lua
@@ -1,10 +1,9 @@
-function _G.Statusline()
-    local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-    -- local branch = vim.fn.system("git branch --show-current err> /dev/null | tr -d '\n'")
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained' }, {
+    callback = function()
+        local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+        -- local branch = vim.fn.system("git branch --show-current err> /dev/null | tr -d '\n'")
 
-    local branch_name = branch == '' and "NoGit" or branch
-
-    return '  ' .. branch_name .. ' ' .. " %f %m %r %h   %=%l/%L "
-end
-
-vim.o.statusline = "%!v:lua.Statusline()"
+        local branch_name = branch == '' and "NoGit" or branch
+        vim.o.statusline = '  ' .. branch_name .. ' ' .. " %f %m %r %h   %=%l/%L "
+    end
+})
