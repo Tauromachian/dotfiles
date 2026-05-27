950847c5e7f9c8851377d8aaf95308dedf08f19d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-05-27 22:22:36 +0200

    Use fnm on cd

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -45,7 +45,7 @@ starship init fish | source
 set fnm_path "$HOME/.local/share/fnm"
 if test -d $fnm_path
     fish_add_path $fnm_path
-    fnm env --shell fish | source
+    fnm env --use-on-cd --shell fish | source
 end
 
 # opencode
