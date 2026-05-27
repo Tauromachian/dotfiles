ee45c10efdacb8aaa6ac93a910473a4abbdd5805
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-05-27 19:33:45 +0200

    Ref/ Substitute selected text instead of word

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -32,7 +32,7 @@ vim.opt.spelllang = "en_us"
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("v", "<leader>s", [["zy:%s/\V<C-r>z/<C-r>z/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
