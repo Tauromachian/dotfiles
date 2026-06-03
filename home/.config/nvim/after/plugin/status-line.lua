44f3b88e5e0828cd225c24889cbef8e8448e010d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-06-03 20:29:08 +0200

    Update status-line

--- home/.config/nvim/after/plugin/status-line.lua
+++ home/.config/nvim/after/plugin/status-line.lua
@@ -1,9 +1,18 @@
-vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained' }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
     callback = function()
-        local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-        -- local branch = vim.fn.system("git branch --show-current err> /dev/null | tr -d '\n'")
+        local result = vim.system(
+            { "git", "branch", "--show-current" },
+            { text = true }
+        ):wait()
 
-        local branch_name = branch == '' and "NoGit" or branch
-        vim.o.statusline = '  ' .. branch_name .. ' ' .. " %f %m %r %h   %=%l/%L "
-    end
+        local branch = ""
+
+        if result.code == 0 then
+            branch = vim.trim(result.stdout)
+        end
+
+        local branch_name = branch ~= "" and branch or "NoGit"
+
+        vim.o.statusline = "  " .. branch_name .. "  %f %m %r %h %=%l/%L "
+    end,
 })
