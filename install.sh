2a17a79ae08bb58cff687b5dbca6d2693ec03d12
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-07-28 13:15:39 +0200

    If dotfiles-personal is downloaded stow it

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ stow_folders=("bash" "zsh" "home")
 for stow_folder in "${stow_folders[@]}"; do
     stow "$stow_folder"
 done
+
+if [ -d "dotfiles-personal/.thunderbird" ]; do
+    stow "dotfiles-personal"
+done
