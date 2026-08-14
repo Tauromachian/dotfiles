115bb0ba6036836c00aa3bcb7c5a26c393de7515
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-14 21:57:20 +0200

    Add workmux completions

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -60,6 +60,8 @@ end
 # opencode
 fish_add_path /home/jose/.opencode/bin
 
+workmux completions fish | source
+
 # pnpm
 set -gx PNPM_HOME "/home/jose/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
