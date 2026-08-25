acbbcfd26080a478143251460b4ff350b419bfae
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-25 16:34:32 +0200

    Delete existing configurations

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+config_paths=("$HOME/.bashrc" "$HOME/.bash_aliases" "$HOME/.tmux.conf" "$HOME/.config/ghostty" "$HOME/.config/nvim" "$HOME/.config/alacritty")
+
+rm -rf "${config_paths[@]}"
+
 stow_folders=("bash" "zsh" "home")
 
 for stow_folder in "${stow_folders[@]}"; do
